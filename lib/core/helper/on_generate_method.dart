abd616350fdce6e9cf47b2078fe549ea1b056422
Mohamed Madkour <madkourmohamed88@gmail.com>
2025-09-07 22:10:06 +0300

    create hotel booking view

--- lib/core/helper/on_generate_method.dart
+++ lib/core/helper/on_generate_method.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:safarni/feature/hotel_booking/presentation/view/hotel_booking_view.dart';
 
 Route<dynamic> onGenerateMethod(RouteSettings settings) {
   switch (settings.name) {
-    case '/':
-      return MaterialPageRoute(builder: (context) => const Scaffold());
+    case HotelBookingView.routeName:
+      return MaterialPageRoute(builder: (context) => const HotelBookingView());
     default:
       return MaterialPageRoute(builder: (context) => Container());
   }
---
+++ lib/feature/hotel_booking/presentation/view/hotel_booking_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HotelBookingView extends StatelessWidget {
+  const HotelBookingView({super.key});
+  static const routeName = 'hotel_booking_view';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
