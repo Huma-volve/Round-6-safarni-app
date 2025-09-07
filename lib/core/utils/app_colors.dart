9b72a4531bdd1950b578c16bc15d5bbbc286566b
Ahmed Mostafa <ahmedmostafa22sh@gmail.com>
2025-09-08 01:53:49 +0300

    add colors & textStyles in utils

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppColors{
+
+  static const Color viewAllColor= Color(0xff1E429F);
+
+  static const Color priceColor= Color(0xff1C64F2);
+
+  static const Color categoryTitleColor= Color(0xff1A56DB);
+
+  static const Color grey900= Color(0xff111928);
+
+  static const Color grey400= Color(0xff9CA3AF);
+
+  static const Color grey500= Color(0xff6B7280);
+
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           home: Scaffold(),
+         // theme: ThemeData(fontFamily: "Poppins"),
         );
       },
     );
