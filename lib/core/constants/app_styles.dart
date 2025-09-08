e88a3b0f679e6e71f5539d596c069c41401eb66f
Menna <mennasyedf@gmail.com>
2025-09-08 19:55:21 +0300

    ui internal tour

---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle Interw700f14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  );
+}
