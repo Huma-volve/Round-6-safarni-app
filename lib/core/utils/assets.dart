5f030cbaf5f448c018f5194676cce11ca927e656
wessam <wsamr600@gmail.com>
2025-09-08 18:35:52 +0300

    make nav bar in onboarding feature

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesArrowIcon
+  /// assets/images/arrow_icon.svg
+  static const String assetsImagesArrowIcon = "assets/images/arrow_icon.svg";
+
+  /// Assets for assetsImagesOnboardingImg1
+  /// assets/images/onboarding_img_1.png
+  static const String assetsImagesOnboardingImg1 =
+      "assets/images/onboarding_img_1.png";
+
+  /// Assets for assetsImagesOnboardingImg2
+  /// assets/images/onboarding_img_2.png
+  static const String assetsImagesOnboardingImg2 =
+      "assets/images/onboarding_img_2.png";
+
+  /// Assets for assetsImagesOnboardingImg3
+  /// assets/images/onboarding_img_3.png
+  static const String assetsImagesOnboardingImg3 =
+      "assets/images/onboarding_img_3.png";
+}
+
+class IconsManager {
+  static String arrorwIcon = "assets/icons/arrow_icon.svg";
+}
